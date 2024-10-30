a207d782659ae3eb31441d8e4fb4fd4cf41ed1ef
itsrauldias <39931516+itsrauldias@users.noreply.github.com>
2024-10-30 17:38:01 -0400

    .

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -13,7 +13,7 @@ export default function Header() {
         <Navbar className={`bg-${sysThemeColor}-50 dark:bg-slate-900`}>
             <Navbar.Brand>
                 <img
-                    src="/habitost.png"
+                    src={`/assets/habitos_${sysThemeColor}.png`}
                     alt="logo"
                     className='mr-2'
                     width={40}
